e89def8d2403f3a2abb69ee4d3c684a22c7e17c7
lucasBlin <lucas.blin@epsi.fr>
2018-10-02 11:13:53 +0200

    latest working version #1

--- src/pages/qr-code/qr-code.ts
+++ src/pages/qr-code/qr-code.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner';
 
 /**
  * Generated class for the QrCodePage page.
@@ -8,6 +9,8 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
  * Ionic pages and navigation.
  */
 
+
+
 @IonicPage()
 @Component({
   selector: 'page-qr-code',
@@ -15,11 +18,21 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class QrCodePage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams,private barcodeScanner: BarcodeScanner) {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad QrCodePage');
+    console.log('ionViewDidLoad Qr  CodePage');
   }
+    qrData = null;
+    scannedCode = null;
+
+    scanCode() {
+      this.barcodeScanner.scan().then(barcodeData => {
+        this.scannedCode = barcodeData.text;
+      }, (err) => {
+          console.log('Error: ', err);
+      });
+    }
 
 }
